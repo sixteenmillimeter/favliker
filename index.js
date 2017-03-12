73de33a613e2653b862aa8a5c915cdd67a368e4e
mmcwilliams <hi@mmcwilliams.com>
2017-03-12 00:27:55 -0500

    Bot is doing what it is set out to do
    Successfully generates tweets and follows rule well enough.
    
    Schedule with a cron job.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const stored = `${dir}store.txt`
 const flat = `${dir}flat.txt`
 const logs = './logs/'
 const results = `${logs}/results.log`
+const start = +new Date()
 
 const cfgProcess = require('./process.json')
 const cfg = {
@@ -21,7 +22,7 @@ const cfg = {
 
 const res = new (winston.Logger)({
 	transports: [
-		//new (winston.transports.Console)(),
+		new (winston.transports.Console)(),
 		new (winston.transports.File)({ filename: results })
 	]
 })
@@ -39,7 +40,19 @@ const idsRaw = fs.readFileSync(stored, 'utf8').split('\n')
 const ids = idsRaw.map(obj => { return obj.split(',')[0] })
 let mostRecent
 
-console.log(`${ids.length} tweets found in storage`)
+function flag (name = null) {
+	let single
+	if (name === null) {
+		return process.argv
+	} else {
+		single = name[0] //first letter of full-length argument
+		if (process.argv.indexOf(single) !== -1 && process.argv.indexOf(name) !== -1) {
+			return false
+		} else {
+
+		} 
+	}
+}
 
 /*
  * favorites/list.json = 75 reqs per 15min windows
@@ -48,6 +61,7 @@ console.log(`${ids.length} tweets found in storage`)
  */
 const favPath = 'favorites/list'
 function getFavs () {
+	res.info('started', {found : ids.length} )
 	if (process.argv.indexOf('-f') !== -1 || process.argv.indexOf('--flat') !== -1) {
 		return runMarkov()
 	}
@@ -73,7 +87,7 @@ function getFavs () {
 		runMarkov()
 	})
 }
-const getFavsPages = (id, cb) => {
+function getFavsPages (id, cb) {
 	let nextPage = false
 	twit.get(favPath, { count : 200,  max_id : id}, (err, data, response) => {
 		if (err) return console.error(err)
@@ -103,7 +117,7 @@ function getRandomArbitrary (min, max) {
 }
 
 function runMarkov () {
-	console.time('markov')
+	const time = +new Date()
 	const minWords = 8
 	const maxWords = 30
 	const markov = new MarkovChain({ 
@@ -124,10 +138,49 @@ function runMarkov () {
 		if (last === ',') {
 			s[s.length - 1] = '.'
 		}
-		console.timeEnd('markov')
-		console.log(s)
-		res.info(s)
+		res.info('markov', {tweet: s, time: (+new Date() - time) })
+		return sendTweet(s)
 	})
+}
+
+function sendTweet (tweet) {
+	tweet = processTweet(tweet)
+	if (flag('silent')) {
+		return console.log(tweet)
+	}
+	//send using twit API
+	//console.log(tweet)
+	return twit.post('statuses/update', { status: tweet }, (err, data, response) => {
+		if (err) {
+			return res.error('sendTweet', { error : err })
+		}
+  		res.info('status/update', { statusCode : response.statusCode, data : data })
+  		res.info('posted', { tweet : tweet, time : (+new Date() - start) })
+  		return process.exit()
+	})
+}
+
+function processTweet (tweet) {
+	//TODO: render image of referenced tweet, skip for now and re-run
+	if (tweet.indexOf('twitter.com/') !== -1 || tweet.indexOf('://t.co/') !== -1) {
+		res.warn('processTweet', 'Contained tweet link, rerunning')
+		return runMarkov()
+	}
+	if (tweet === undefined || tweet === 'undefined' || typeof tweet === 'undefined') {
+		res.warn('processTweet', 'Tweet was undefined, rerunning')
+		return runMarkov()
+	}
+
+	tweet = cleanTweet(tweet)
+	return tweet
+}
+
+function cleanTweet (tweet) {
+	//Replace @'s with @  to prevent spamming people
+	const atRe = new RegExp('@', 'g')
+	tweet = tweet.replace(atRe, '○') //use &cir; instead of @
+
+	return tweet
 }
 
 getFavs()
